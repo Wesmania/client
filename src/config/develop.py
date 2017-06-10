d9937461aa235d47b689614f45776baee5d40b05
Martin Müllenhaupt <mm+git@netlair.de>
2017-06-10 21:39:59 +0200

    make console logging a setting (which defaults to true for development environment)

--- src/config/develop.py
+++ src/config/develop.py
@@ -2,6 +2,7 @@ from .production import defaults as production_defaults
 
 defaults = production_defaults.copy()
 defaults['host'] = 'test.faforever.com'
+defaults['client/logs/console'] = True
 
 # FIXME: Temporary fix for broken https config on test server
 # Turns off certificate verification entirely
